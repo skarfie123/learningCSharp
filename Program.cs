2cf64435d000dbff776533086c95c3711d24eafb
Rahul Pai <rahulpai@rocketmail.com>
2020-06-19 20:07:48 +0100

    ms intro

--- Program.cs
+++ Program.cs
@@ -1,12 +1,215 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace learningC_
+namespace learningCSharp
 {
     class Program
     {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+
+            // addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+        }
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+            Console.WriteLine(h);
+        }
+        static void IntegerPrecision()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+        static void Double()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+        }
+        static void Decimal()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+        static void Loops()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.Write(counter);
+                counter++;
+            }
+            System.Console.WriteLine();
+            //do minimum one time
+            counter = 0;
+            do
+            {
+                Console.Write(counter);
+                counter++;
+            } while (counter < 10);
+            System.Console.WriteLine();
+            for (int row = 1; row < 4; row++)
+            {
+                for (char column = 'a'; column < 'd'; column++)
+                {
+                    Console.Write($"({row}, {column}) ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+        static void WorkingWithStrings()
+        {
+            var names = new List<string> { "<name>", "Ana", "Felipe" }; //using System.Collections.Generic for List
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.Write(name.ToUpper() + " ");
+            }
+            System.Console.WriteLine();
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.Write(name.ToUpper() + " ");
+            }
+            System.Console.WriteLine();
+        }
+        static void BankingClasses()
+        {
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(">>>Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(">>>Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(account.GetAccountHistory());
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            WorkingWithIntegers();
+            OrderPrecedence();
+            IntegerPrecision();
+            Double();
+            Decimal();
+            Loops();
+            WorkingWithStrings();
+            BankingClasses();
         }
     }
 }
